1e0dcb3635e93cd4f2ac6e38913eb8ade0cd719d
tyVespA <migliavacca.marco08@gmail.com>
2024-12-27 10:00:45 +0100

    Add styles, clean up functions

--- src/app/components/ImageSlider.tsx
+++ src/app/components/ImageSlider.tsx
@@ -7,27 +7,29 @@ export default function ImageSlider({ imageUrls }) {
   const [imageIndex, setImageIndex] = useState(0);
 
   function showNextImage() {
-    if (imageIndex == imageUrls.length - 1) {
-      setImageIndex(0);
-    } else {
-      setImageIndex(imageIndex + 1);
-    }
+    setImageIndex((index) => {
+      if (index == imageUrls.length - 1) return 0;
+      return index + 1;
+    });
   }
 
   function showPreviousImage() {
-    if (imageIndex == 0) {
-      setImageIndex(imageUrls.length - 1);
-    } else {
-      setImageIndex(imageIndex - 1);
-    }
+    setImageIndex((index) => {
+      if (index == 0) return imageUrls.length - 1;
+      return index - 1;
+    });
   }
   return (
     <div className={styles.imageSliderContainer}>
       <button onClick={showPreviousImage}>
         <ArrowBigLeft />
       </button>
-      <img src={imageUrls[imageIndex]} alt="" className={styles.image} />
-      <button onClick={showNextImage}>
+      <img
+        src={imageUrls[imageIndex]}
+        alt={imageUrls[imageIndex]}
+        className={styles.image}
+      />
+      <button onClick={showNextImage} className={styles.nextButton}>
         <ArrowBigRight />
       </button>
     </div>
